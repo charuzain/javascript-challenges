be6dcf060c45b532b748f7446e698f924eb90629
charuzain <charuabhizain@gmail.com>
2022-02-05 22:26:14 -0500

    initial commit

---
+++ challenge1.js
@@ -0,0 +1,5 @@
+
+// This function accepts an array and a searchTerm (this will always be a primitive value) and returns the number of times the searchTerm appears in the array.
+
+// frequency([1, 2, 3, 4, 4, 4], 4); // 3
+// frequency([true, false, true, true], false); // 1
